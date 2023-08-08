4031e200c204cd985d08192841715d73fe9ae101
Dimitar-LDimitrov <81306670+Dimitar-LDimitrov@users.noreply.github.com>
2023-08-08 14:54:30 +0300

    first commit

---
+++ Augmented Reality App/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  Augmented Reality App
+//
+//  Created by Dimitar Dimitrov on 8.08.23.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Create a new scene
+       // let scene = SCNScene(named: "art.scnassets/ship.scn")!
+       //
+       // // Set the scene to the view
+       // sceneView.scene = scene
+        
+        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.blue
+        
+        cube.materials = [material]
+        print("Something")
+        
+        let node = SCNNode()
+        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+        node.geometry = cube
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        sceneView.autoenablesDefaultLighting = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        print("ARWorldTrackingConfiguration is supported?: \(ARWorldTrackingConfiguration.isSupported)")
+       
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+}
